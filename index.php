48efe59a0624e560a6b4ae7cde182b1b71898dd3
Matteo Ferrarese <matteo.ferrarese@yahoo.it>
2021-01-18 14:08:44 +0100

    Istruzioni

---
+++ index.php
@@ -0,0 +1,7 @@
+TODO TODAYGOAL: php-oop-dipendenti
+REPO: 
+creare 3 classi per rappresentare la seguente realta':
+- persona
+- dipendente
+- bosscercare inoltre di sciegliere alcune variabili di istanza (max 3 o 4 per classe) che possono avere senso in questa realta' e decidere le relazione di parantela (chi estende chi);
+per ogni classe definire eventuale classe padre, variabili di istanza, costruttore, proprieta' e toString;instanziando le varie classi provare a stampare cercando di ottenere un log sensato
